d41d67c7cddce9d2bdc67637c67fcab89d9a4298
Yogender Kulshrestha <yogenderkulshrestha0001@gmail.com>
2021-09-09 18:25:19 +0530

    update pusher

---
+++ app/Events/ChatEvent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Chat;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChatEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $chat;
+
+    public function __construct(Chat $chat)
+    {
+        $this->chat = $chat;
+    }
+
+    public function broadcastOn()
+    {
+        return new PresenceChannel('chat');
+    }
+}
